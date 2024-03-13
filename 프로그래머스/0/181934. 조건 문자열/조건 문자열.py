abc43849514a60f9c2a7403f483407909aa04393
Seonggyu Lee <93371421+lsk74669@users.noreply.github.com>
2024-03-13 23:39:04 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,12 @@
+def solution(ineq, eq, n, m):
+    if ineq == '<':
+        if eq == '=':
+            return int(n <= m)
+        else:
+            return int(n < m)
+    else:
+        if eq == '=':
+            return int(n >= m)
+        else:
+            return int(n > m)
+    return 0
