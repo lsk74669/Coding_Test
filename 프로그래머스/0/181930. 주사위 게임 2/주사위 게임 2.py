913f09dcb4dbcc23df2c577345e266aa7282b400
Seonggyu Lee <93371421+lsk74669@users.noreply.github.com>
2024-03-19 01:06:12 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,7 @@
+def solution(a, b, c):
+    if a == b == c:
+        return (a + b + c) * ( a**2 + b**2 + c** 2) * (a**3 + b**3 + c**3)
+    elif (a == b and a !=c) or (a == c and a !=b) or (b == c and b !=a):
+        return (a + b + c) * ( a**2 + b**2 + c** 2)
+    else:
+        return (a + b + c)
