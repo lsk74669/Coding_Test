48231e53e893430789f13083b8788edccb72c305
Seonggyu Lee <93371421+lsk74669@users.noreply.github.com>
2024-04-23 18:34:58 +0900

    [level 0] Title: l로 만들기, Time: 18.33 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for i in myString:
+        if ord(i) < ord('l'):
+            answer += 'l'
+        else:
+            answer += i
+            
+    return answer
