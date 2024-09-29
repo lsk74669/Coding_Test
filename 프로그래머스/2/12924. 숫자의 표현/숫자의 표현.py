6f89bd38869bb298ded137feda3a4c1ef2bc5304
Seonggyu Lee <93371421+lsk74669@users.noreply.github.com>
2024-09-29 23:50:14 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,10 @@
+def solution(n):
+    cnt = 0
+    k = 1
+    
+    while k * (k - 1) // 2 < n:
+        if (n - (k * (k - 1)) // 2) % k == 0:
+            cnt += 1
+        k += 1
+        
+    return cnt
