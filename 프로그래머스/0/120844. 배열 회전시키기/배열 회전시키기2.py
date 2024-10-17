f8e00e36f9cbe9e9dae022fc6333227e859159e7
Seonggyu Lee <93371421+lsk74669@users.noreply.github.com>
2024-10-17 19:35:21 +0900

    Create 배열 회전시키기2.py

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기2.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+def solution(numbers, direction):
+    q = deque(numbers)
+    
+    if direction == 'right':
+        q.appendleft(q.pop())
+    else:
+        q.append(q.popleft())
+    return list(q)
